3fd58021bce3a508053d6e86647bbe3e1c97a2ea
Pabloarey <112872312+Pabloarey@users.noreply.github.com>
2025-02-12 14:58:28 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,6 +13,7 @@ st.set_page_config(initial_sidebar_state="collapsed",    page_title="SIE",
 pages = ["Home","Tablero","Mapa"]
 parent_dir = os.path.dirname(os.path.abspath(__file__))
 logo_path = os.path.join(parent_dir, "./logos/SIE-SVG.svg")
+urls = {"GitHub": "https://github.com/gabrieltempass/streamlit-navigation-bar"}
 styles = {
     "nav": {
         "background-color": "royalblue",
@@ -40,6 +41,7 @@ options = {
 page = st_navbar(
     pages,
     logo_path=logo_path,
+    urls=urls,
     styles=styles,
     options=options,
 )
