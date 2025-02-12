923e4fdfd414d26a679df7f1bc2ce94edd3e0ad0
Pabloarey <112872312+Pabloarey@users.noreply.github.com>
2025-02-12 15:21:23 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import os
 import pandas as pd
 import streamlit as st
 from streamlit_navigation_bar import st_navbar
-import ..pages as pg
+import pages as pg
 
  
 st.set_page_config(initial_sidebar_state="collapsed",    page_title="SIE",
