3ae84eb816300b86a5870244470c3167f3d497f1
Pabloarey <112872312+Pabloarey@users.noreply.github.com>
2025-02-13 14:39:52 -0300

    Update home.py

--- pages/home.py
+++ pages/home.py
@@ -5,7 +5,7 @@ from sklearn.preprocessing import StandardScaler
 import plotly.graph_objects as go
 
 def show_home():
-    st.header('Herramienta aplicada a __Procesos__ __Electorales__')
+    st.header('Herramienta aplicada a Procesos Electorales')
     st.image("./logos/SIE.jpeg", caption=None, width=None, use_column_width=None, clamp=False, channels="RGB", output_format="auto")
     
     
